69d37a2889941b83f41af464a7f2dbd70997d262
G_Bhavani_Shankar <112201026@smail.iitpkd.ac.in>
2025-08-25 21:53:17 +0530

    Sum upto n script

---
+++ Class/week-2/add.sh
@@ -0,0 +1,25 @@
+read -p "Enter a number: " n
+
+if ! [[ "$n" =~ ^[0-9]+$ ]]; then
+    echo "Error: Please enter a valid positive number"
+    exit 1
+fi
+
+if [ "$n" -lt 0 ]; then
+    echo "Error : Please enter only positive numbers"
+    exit 1
+fi
+
+sum_n() {
+    num=$1
+    sum=0
+    while [ "$num" -gt 0 ]; do
+        sum=$((sum + num))
+        num=$((num - 1))
+    done
+    echo $sum
+}
+
+sum=$(sum_n "$n")
+
+echo "Sum of first $n natural numbers is: $sum"
